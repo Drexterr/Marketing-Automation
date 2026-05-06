81ec450db18f47931ce060f298fd2e25dc6a4c77
Bharat Jain <bharatbanthia2002@gmail.com>
2026-05-06 11:55:45 +0530

    chore: initial project structure and logger

---
+++ src/utils/logger.js
@@ -0,0 +1,21 @@
+import winston from 'winston';
+import path from 'path';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: path.join('logs', 'automation.log') }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
+  ]
+});
+
+export default logger;
